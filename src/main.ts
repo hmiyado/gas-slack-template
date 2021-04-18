b503dff8f1b49b64bdef7bd11a9a0c71eab669db
miyado <hmiyado@gmail.com>
2021-04-18 17:43:08 +0900

    extract Slack as a file

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,3 @@
-class SlackClient {
-  webhook = Secrets.SLACK_INCOMING_WEBHOOK
-
-  post() {
-    const body = {
-      text: 'Hello, GAS!'
-    }
-    const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
-      method: 'post',
-      payload: `payload=${JSON.stringify(body)}`
-    };
-
-    UrlFetchApp.fetch(this.webhook, options)
-  }
-}
-
 function main() {
-  (new SlackClient()).post()
+  Slack.post('Hello, GAS!')
 }
---
+++ src/slack.ts
@@ -0,0 +1,15 @@
+namespace Slack {
+    const webhook = Secrets.SLACK_INCOMING_WEBHOOK
+
+    export const post = (text: string) => {
+      const body = {
+        text
+      }
+      const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+        method: 'post',
+        payload: `payload=${JSON.stringify(body)}`
+      };
+  
+      UrlFetchApp.fetch(webhook, options)
+    }     
+}
