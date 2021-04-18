e6e24aa1a1bbb43995bba946ad8d2c9f89395d7c
miyado <hmiyado@gmail.com>
2021-04-18 16:42:08 +0900

    use eslint/prettier/huskey/lint-staged

--- hello.ts
+++ hello.ts
@@ -1,8 +1,8 @@
 const greeter = (person: string) => {
-    return `Hello, ${person}!`;
-  }
-  
-  function testGreeter() {
-    const user = 'Grant';
-    Logger.log(greeter(user));
-  }
+  return `Hello, ${person}!`;
+};
+
+function testGreeter() {
+  const user = "Grant";
+  Logger.log(greeter(user));
+}
